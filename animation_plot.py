9b899845a3bf57d8de2f45a332556408718dc18c
Santosh Balajee Banisetty <santoshbanisetty@nevada.unr.edu>
2019-07-11 13:50:58 -0700

    Update function for dictionary (people locations added)

--- animation_plot.py
+++ animation_plot.py
@@ -8,11 +8,11 @@ rect_width = 0.5
 rect_height = 0.3
 square_length = 0.2
 
-scenario_dist =	{
+scenario_dist = {
   "queue": [(1, 1, 90), (2, 2, 90), (3, 3, 90)],
   "art": [(1, 1, 180)],
   "formation": [(1, 1, 90), (2, 2, 180), (3, 3, 270)]
-}
+}# initial dummy dictionary
 
 parser = argparse.ArgumentParser()
 
@@ -23,6 +23,19 @@ parser.add_argument('--save', help='1 to save the animation')
 args = parser.parse_args()
 print(args)
 
+#Update function to replace dictionary values
+def update_people(scenario):
+    if scenario == "queue":
+        scenario_dist[scenario] = [(11, 12, 90), (13, 12, 90), (15, 12, 90)]
+    if scenario == "formation":
+        scenario_dist[scenario] = [(11, 12, 90), (13, 14, 0), (15, 12, 270)]
+    if scenario == "art":
+        scenario_dist[scenario] = [(22.37, 10.96, 90)]
+    else:
+        print("Invalid scenario")
+
+update_people(args.scenario)
+
 people_pose = scenario_dist[args.scenario]
 num_people = len(people_pose)
 print(people_pose)
@@ -31,7 +44,7 @@ fig = plt.figure()
 fig.set_dpi(100)
 fig.set_size_inches(7, 6.5)
 
-ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
+ax = plt.axes(xlim=(0, 25), ylim=(0, 25))
 ax.grid(color='k', linestyle=':', linewidth=0.25)
 patch = Ellipse(xy=(0.5, 0.5), width=0.5, height=0.2, angle=60)
 patch1 = Circle((5, -5), 0.1, fc='k')
@@ -53,7 +66,7 @@ def init():
     for person in people:
         ax.add_patch(person)
     for head in heads:
-    	  ax.add_patch(head)
+          ax.add_patch(head)
     patch.center = (5, 5)
     patch1.center = (5, 5)
     rect.xy=(4-rect_width/2, 4-rect_height/2) #adjust for lower corner of the rectangle
