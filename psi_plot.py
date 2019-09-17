df1d0b2e47ff64e8033c6d6ecad2ad1792242fc7
Santosh Balajee Banisetty <santoshbanisetty@nevada.unr.edu>
2019-09-16 21:00:43 -0700

    psi plots initial commit

--- psi_plot.py
+++ psi_plot.py
@@ -26,7 +26,8 @@ scenario_dist = {
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('file', help='Path to the file')
+parser.add_argument('file1', help='Path to the traditional trajectory file')
+parser.add_argument('file2', help='Path to the social trajectory file')
 parser.add_argument('scenario', help='SAN scenario[queue, art, formation]')
 parser.add_argument('--save', help='1 to save the animation')
 
@@ -54,7 +55,8 @@ people_pose = scenario_dist[args.scenario]
 num_people = len(people_pose)
 print(people_pose)
 
-data = get_data(args.file)
+data = get_data(args.file1)
+san_data = get_data(args.file2)
 print (data.shape)
 animation_count = data.shape[0]
 
@@ -90,72 +92,74 @@ people = [Ellipse(xy=(people_pose[i][0], people_pose[i][1]),
 heads = [Circle((people_pose[i][0], people_pose[i][1]), radius=0.2, fc='k')
         for i in range(num_people)]
 
-def init():
-    for person in people:
-        ax.add_patch(person)
-    for head in heads:
-        ax.add_patch(head)
+for person in people:
+    ax.add_patch(person)
+for head in heads:
+    ax.add_patch(head)
 
-    t_x = 5
-    t_y = 5
+t_x = 5
+t_y = 5
 
-    rect.xy=(t_x, t_y) 
-    ax.add_patch(rect)
-    # ax.add_patch(furniture)
-    ax.add_patch(frame1)
-    ax.add_patch(frame2)
-    ax.add_patch(frame3)
-    return rect, frame1, frame2, frame3
+rect.xy=(data[0, 0]-0.2, data[0, 1]-0.2) 
+ax.add_patch(rect)
+# ax.add_patch(furniture)
+ax.add_patch(frame1)
+ax.add_patch(frame2)
+ax.add_patch(frame3)
 
-def animate(i):
-    # print (i)
-    global pre_y, pre_x, dq
-    x, y = rect.xy
-    # x = 5 + 3 * np.sin(np.radians(i))
-    # y = 5 + 3 * np.cos(np.radians(i))
-    x = data[i, 0]
-    y = data[i, 1]
-    alpha = np.rad2deg(np.arctan2(y - pre_y, x - pre_x))
-    dq.append(alpha)
-    angle = np.mean(dq)
-    rect.xy = (x - np.sqrt(2)*rect_height/2*np.cos(np.radians(45+angle)), y -np.sqrt(2)*rect_height/2*np.sin(np.radians(45+angle)))
-    rect.angle = angle
+ax.plot(data[:, 0], data[:, 1], 'k', linewidth=2.0, label='Traditional Trajectory')
+ax.plot(san_data[:, 0], san_data[:, 1], 'b', linewidth=2.0, label='Social Trajectory')
+
+# def animate(i):
+#     # print (i)
+#     global pre_y, pre_x, dq
+#     x, y = rect.xy
+#     # x = 5 + 3 * np.sin(np.radians(i))
+#     # y = 5 + 3 * np.cos(np.radians(i))
+#     x = data[i, 0]
+#     y = data[i, 1]
+#     alpha = np.rad2deg(np.arctan2(y - pre_y, x - pre_x))
+#     dq.append(alpha)
+#     angle = np.mean(dq)
+#     rect.xy = (x - np.sqrt(2)*rect_height/2*np.cos(np.radians(45+angle)), y -np.sqrt(2)*rect_height/2*np.sin(np.radians(45+angle)))
+#     rect.angle = angle
 
 
-    pre_x = x
-    pre_y = y
-    lst = []
-    lst.append(rect)
+#     pre_x = x
+#     pre_y = y
+#     lst = []
+#     lst.append(rect)
 
-    lst.append(frame1)
-    lst.append(frame2)
-    lst.append(frame3)
+#     lst.append(frame1)
+#     lst.append(frame2)
+#     lst.append(frame3)
 
-    for index, person in enumerate(people):
-        if random.uniform(0, 9) <= 1:
-            if person.angle == 90 or person.angle == 270:
-                person.center = (people_pose[index][0], people_pose[index][1]+random.uniform(-0.03, 0.03))
-                heads[index].center = (people_pose[index][0], people_pose[index][1]+random.uniform(-0.03, 0.03))
-            else:
-                person.center = (people_pose[index][0]+random.uniform(-0.03, 0.03), people_pose[index][1])
-                heads[index].center = (people_pose[index][0]+random.uniform(-0.03, 0.03), people_pose[index][1])
-        lst.append(person)
-        lst.append(heads[index])
-    return lst
+#     for index, person in enumerate(people):
+#         if random.uniform(0, 9) <= 1:
+#             if person.angle == 90 or person.angle == 270:
+#                 person.center = (people_pose[index][0], people_pose[index][1]+random.uniform(-0.03, 0.03))
+#                 heads[index].center = (people_pose[index][0], people_pose[index][1]+random.uniform(-0.03, 0.03))
+#             else:
+#                 person.center = (people_pose[index][0]+random.uniform(-0.03, 0.03), people_pose[index][1])
+#                 heads[index].center = (people_pose[index][0]+random.uniform(-0.03, 0.03), people_pose[index][1])
+#         lst.append(person)
+#         lst.append(heads[index])
+#     return lst
 
-anim = animation.FuncAnimation(fig, animate, 
-                               init_func=init, 
-                               frames=animation_count, 
-                               interval=100,
-                               blit=True)
+# anim = animation.FuncAnimation(fig, animate, 
+#                                init_func=init, 
+#                                frames=animation_count, 
+#                                interval=100,
+#                                blit=True)
 
 ax.legend()
 
 if args.save == "1":
     #Save the animation
-    anim.save('animation.mp4', fps=30, 
-          extra_args=['-vcodec', 'h264', 
-                      '-pix_fmt', 'yuv420p'])
+    # anim.save('animation.mp4', fps=30, 
+    #       extra_args=['-vcodec', 'h264', 
+    #                   '-pix_fmt', 'yuv420p'])
+    pass
 else:
     #Show animation
     plt.show()
